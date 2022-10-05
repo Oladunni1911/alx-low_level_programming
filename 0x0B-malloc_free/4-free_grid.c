1dc88c98db842ff8fe1807231fab3a5d6aa7a6e1
Oladunni1911 <olayinka.oladunni@yahoo.com>
2022-10-05 05:20:51 -0700

    Eminado

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,11 +3,11 @@
 /**
  * free_grid - frees a 2d array
  * @grid: memory block to be freed
- * height: height of the array
+ * @height: height of the array
  * Return: returns void
  */
 
-void free_grid(int **grid, int *height)
+void free_grid(int **grid, int height)
 {
 	int i;
 
